f7b2209c4539288b049ca2aec38c06e9d8f2fd19
Dragos Rotaru <r.dragos0@gmail.com>
2022-05-25 20:16:39 +0300

    better grouping

--- benches/rng.rs
+++ benches/rng.rs
@@ -42,45 +42,43 @@ fn rng_fill(c: &mut Criterion) {
 
 fn rng_next64(c: &mut Criterion) {
     let mut group = c.benchmark_group("rng_next_u64");
-    group.bench_function("chacha8", |b| {
-        let mut rng = ChaCha8Rng::seed_from_u64(42);
-        let n: u64 = 1000;
-        b.iter(|| {
-            for _ in 0..n {
-                let _ = rng.next_u64();
-            }
-        })
-    });
+    for n in [1000] {
+        group.bench_function("chacha8", |b| {
+            let mut rng = ChaCha8Rng::seed_from_u64(42);
+            b.iter(|| {
+                for _ in 0..n {
+                    let _ = rng.next_u64();
+                }
+            })
+        });
 
-    group.bench_function("chacha12", |b| {
-        let mut rng = ChaCha12Rng::seed_from_u64(42);
-        let n: u64 = 1000;
-        b.iter(|| {
-            for _ in 0..n {
-                let _ = rng.next_u64();
-            }
-        })
-    });
+        group.bench_function("chacha12", |b| {
+            let mut rng = ChaCha12Rng::seed_from_u64(42);
+            b.iter(|| {
+                for _ in 0..n {
+                    let _ = rng.next_u64();
+                }
+            })
+        });
 
-    group.bench_function("chacha20", |b| {
-        let mut rng = ChaCha20Rng::seed_from_u64(42);
-        let n: u64 = 1000;
-        b.iter(|| {
-            for _ in 0..n {
-                let _ = rng.next_u64();
-            }
-        })
-    });
+        group.bench_function("chacha20", |b| {
+            let mut rng = ChaCha20Rng::seed_from_u64(42);
+            b.iter(|| {
+                for _ in 0..n {
+                    let _ = rng.next_u64();
+                }
+            })
+        });
 
-    group.bench_function("aes", |b| {
-        let mut rng = AesRng::from_seed([0u8; SEED_SIZE]);
-        let n: u64 = 1000;
-        b.iter(|| {
-            for _ in 0..n {
-                let _ = rng.next_u64();
-            }
-        })
-    });
+        group.bench_function("aes", |b| {
+            let mut rng = AesRng::from_seed([0u8; SEED_SIZE]);
+            b.iter(|| {
+                for _ in 0..n {
+                    let _ = rng.next_u64();
+                }
+            })
+        });
+    }
 }
 
 criterion_group!(benches, rng_fill, rng_next64);
