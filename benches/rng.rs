b11f01c225a82c212d9d6c06daeac128f88c64b3
Dragos Rotaru <r.dragos0@gmail.com>
2022-05-25 18:00:26 +0300

    added more benches and more tests

--- benches/rng.rs
+++ benches/rng.rs
@@ -1,12 +1,12 @@
 use aes_prng::{AesRng, SEED_SIZE};
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::{RngCore, SeedableRng};
-use rand_chacha::ChaCha20Rng;
+use rand_chacha::{ChaCha12Rng, ChaCha20Rng, ChaCha8Rng};
 
 fn aes_rng(c: &mut Criterion) {
-    c.bench_function("aes_rng_fill_bytes", |b| {
+    c.bench_function("aes_rng_fill_2MB", |b| {
         let mut rng = AesRng::from_seed([0u8; SEED_SIZE]);
-        let mut output = vec![0u8; 2 * 1024 * 1024];
+        let mut output = vec![0u8; 2 * 1000 * 1000];
         b.iter(|| {
             rng.try_fill_bytes(&mut output).unwrap();
         })
@@ -24,15 +24,51 @@ fn aes_rng(c: &mut Criterion) {
 }
 
 fn chacha_rng(c: &mut Criterion) {
-    c.bench_function("chacha_rng_fill_bytes", |b| {
-        let mut rng = ChaCha20Rng::seed_from_u64(42);
+    c.bench_function("chacha8_rng_fill_2MB", |b| {
+        let mut rng = ChaCha8Rng::seed_from_u64(42);
         let mut output = vec![0u8; 2 * 1024 * 1024];
         b.iter(|| {
             rng.try_fill_bytes(&mut output).unwrap();
         })
     });
 
-    c.bench_function("chacha_rng_next_u64", |b| {
+    c.bench_function("chacha12_rng_fill_2MB", |b| {
+        let mut rng = ChaCha12Rng::seed_from_u64(42);
+        let mut output = vec![0u8; 2 * 1000 * 1000];
+        b.iter(|| {
+            rng.try_fill_bytes(&mut output).unwrap();
+        })
+    });
+
+    c.bench_function("chacha20_rng_fill_2MB", |b| {
+        let mut rng = ChaCha20Rng::seed_from_u64(42);
+        let mut output = vec![0u8; 2 * 1000 * 1000];
+        b.iter(|| {
+            rng.try_fill_bytes(&mut output).unwrap();
+        })
+    });
+
+    c.bench_function("chacha8_rng_next_u64", |b| {
+        let mut rng = ChaCha8Rng::seed_from_u64(42);
+        let n: u64 = 1000;
+        b.iter(|| {
+            for _ in 0..n {
+                let _ = rng.next_u64();
+            }
+        })
+    });
+
+    c.bench_function("chacha12_rng_next_u64", |b| {
+        let mut rng = ChaCha12Rng::seed_from_u64(42);
+        let n: u64 = 1000;
+        b.iter(|| {
+            for _ in 0..n {
+                let _ = rng.next_u64();
+            }
+        })
+    });
+
+    c.bench_function("chacha20_rng_next_u64", |b| {
         let mut rng = ChaCha20Rng::seed_from_u64(42);
         let n: u64 = 1000;
         b.iter(|| {
@@ -51,7 +87,7 @@ fn thread_rng(c: &mut Criterion) {
             rng.fill_bytes(&mut output);
         })
     });
-    c.bench_function("thread_rng_16B", |b| {
+    c.bench_function("thread_rng_16B_no_setup", |b| {
         b.iter(|| {
             let mut rng = rand::thread_rng();
             let mut output = vec![0u8; 16];
@@ -71,24 +107,5 @@ fn thread_rng(c: &mut Criterion) {
     });
 }
 
-fn getrandom_rng(c: &mut Criterion) {
-    c.bench_function("get_random_rng_16B", |b| {
-        b.iter(|| {
-            let mut output = vec![0u8; 16];
-            getrandom::getrandom(&mut output).expect("failed to get randomness");
-        })
-    });
-
-    c.bench_function("get_random_rng_16B_10000", |b| {
-        let n: u64 = 10000;
-        let mut output = vec![0u8; 16];
-        b.iter(|| {
-            for _ in 0..n {
-                getrandom::getrandom(&mut output).expect("failed to get randomness");
-            }
-        })
-    });
-}
-
-criterion_group!(benches, thread_rng, getrandom_rng, chacha_rng, aes_rng);
+criterion_group!(benches, thread_rng, chacha_rng, aes_rng);
 criterion_main!(benches);
