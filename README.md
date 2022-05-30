a8a209d22d014e5822ffa5d3701147308e5aeba0
Morten Dahl <mortendahlcs@gmail.com>
2022-05-30 11:36:15 +0200

    ..

--- README.md
+++ README.md
@@ -25,7 +25,36 @@ https://github.com/RustCrypto/block-ciphers/tree/master/aes) dependency.
 AES-PRNG is built with the `-Ctarget-feature=+aes` feature enabled by default
 to get the benefit of AES-NI instructions for speeding up the PRNG calls.
 
-# License
+## Benchmarks
+
+On AMD Ryzen 9 3900X:
+
+```
+$ cargo bench -- rng_fill
+
+rng_fill/chacha8/10     time:   [1.7716 us 1.7729 us 1.7742 us]
+rng_fill/chacha12/10    time:   [2.4794 us 2.4810 us 2.4831 us]
+rng_fill/chacha20/10    time:   [3.9333 us 3.9391 us 3.9444 us]
+rng_fill/aes/10         time:   [2.2231 us 2.2232 us 2.2234 us]
+
+rng_fill/chacha8/100    time:   [18.266 us 18.269 us 18.271 us]
+rng_fill/chacha12/100   time:   [24.603 us 24.607 us 24.610 us]
+rng_fill/chacha20/100   time:   [38.965 us 38.970 us 38.974 us]
+rng_fill/aes/100        time:   [24.080 us 24.113 us 24.144 us]
+
+rng_fill/chacha8/1000   time:   [176.70 us 176.71 us 176.73 us]
+rng_fill/chacha12/1000  time:   [248.39 us 248.41 us 248.44 us]
+rng_fill/chacha20/1000  time:   [391.49 us 391.68 us 391.90 us]
+rng_fill/aes/1000       time:   [225.52 us 225.53 us 225.54 us]
+```
+
+On Apple M1 Max:
+
+```
+$ 
+```
+
+## License
 
 AES-PRNG is distributed under the terms of Apache License (Version 2.0).
 
--- benches/rng.rs
+++ benches/rng.rs
@@ -4,37 +4,34 @@ use rand::{RngCore, SeedableRng};
 use rand_chacha::{ChaCha12Rng, ChaCha20Rng, ChaCha8Rng};
 
 fn rng_fill(c: &mut Criterion) {
-    fn rng_bencher<T: RngCore>(b: &mut Bencher, rng: &mut T, buffer_size: usize) {
+    fn rng_bencher<T: RngCore>(b: &mut Bencher, rng: &mut T, buffer_size_kb: usize) {
+        let buffer_size = buffer_size_kb * 1024;
         let mut output = vec![0u8; buffer_size];
         b.iter(|| {
-            rng.try_fill_bytes(&mut output).unwrap();
+            rng.fill_bytes(&mut output);
         })
     }
 
     let mut group = c.benchmark_group("rng_fill");
-    for buffer_size in [10, 100, 1000] {
-        group.bench_function(BenchmarkId::new("chacha8", buffer_size), |b| {
+    for buffer_size_kb in [10, 100, 1000] {
+        group.bench_function(BenchmarkId::new("chacha8", buffer_size_kb), |b| {
             let mut rng = ChaCha8Rng::seed_from_u64(42);
-            let buffer_size = buffer_size * 1000;
-            rng_bencher(b, &mut rng, buffer_size)
+            rng_bencher(b, &mut rng, buffer_size_kb)
         });
 
-        group.bench_function(BenchmarkId::new("chacha12", buffer_size), |b| {
+        group.bench_function(BenchmarkId::new("chacha12", buffer_size_kb), |b| {
             let mut rng = ChaCha12Rng::seed_from_u64(42);
-            let buffer_size = buffer_size * 1000;
-            rng_bencher(b, &mut rng, buffer_size)
+            rng_bencher(b, &mut rng, buffer_size_kb)
         });
 
-        group.bench_function(BenchmarkId::new("chacha20", buffer_size), |b| {
+        group.bench_function(BenchmarkId::new("chacha20", buffer_size_kb), |b| {
             let mut rng = ChaCha20Rng::seed_from_u64(42);
-            let buffer_size = buffer_size * 1000;
-            rng_bencher(b, &mut rng, buffer_size)
+            rng_bencher(b, &mut rng, buffer_size_kb)
         });
 
-        group.bench_function(BenchmarkId::new("aes", buffer_size), |b| {
+        group.bench_function(BenchmarkId::new("aes", buffer_size_kb), |b| {
             let mut rng = AesRng::from_seed([0u8; SEED_SIZE]);
-            let buffer_size = buffer_size * 1000;
-            rng_bencher(b, &mut rng, buffer_size)
+            rng_bencher(b, &mut rng, buffer_size_kb)
         });
     }
 }
